fd2a8f593ec42b9043314fe8ed00c9fe7aa28ff7
Lucas Wawrzyniak <lucas.wawrzyniak@gmail.com>
2019-04-05 09:46:18 +0200

    fix(view): Home page extends layout + commented code clean up
    Having Home page extending layout prevents us from setting twice some
    features like notifications from socket.

--- src/Repository/ACL/AdminRepository.php
+++ src/Repository/ACL/AdminRepository.php
@@ -27,33 +27,4 @@ class AdminRepository extends ServiceEntityRepository
         $this->container = $container;
         $this->_em = $this->container->get('doctrine.orm.acl_entity_manager');
     }
-
-    // /**
-    //  * @return Admin[] Returns an array of Admin objects
-    //  */
-    /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('a.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?Admin
-    {
-        return $this->createQueryBuilder('a')
-            ->andWhere('a.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
